111ae2d7a07d294a31c0b5ce110ea3f691f10e91
danielsafari143 <danielsafari143@gmail.com>
2023-06-08 20:36:55 +0200

    ADD COMPONENTS

---
+++ src/components/Forms.jsx
@@ -0,0 +1,12 @@
+const Forms = () => (
+  <div>
+    <h3>ADD NEW BOOK</h3>
+    <div>
+      <input type="text" placeholder="Book title" />
+      <input type="text" placeholder="Auther" />
+      <input type="button" value="ADD BOOK" />
+    </div>
+  </div>
+);
+
+export default Forms;
