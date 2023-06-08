4b8965bd99c2c355fc443fb13c61406b3814c28c
danielsafari143 <danielsafari143@gmail.com>
2023-06-08 17:13:05 +0200

    changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    <header>
+        <nav>
+            <Link to='/'>BOOKS</Link>
+            <Link to='categories'>CATEGORIES</Link>
+        </nav>
+    </header>
+}
+
+export default Navbar;
