90ac026d4433e926180c673b70fa255ce1481ad8
danielsafari143 <danielsafari143@gmail.com>
2023-06-12 23:50:44 +0200

    USE REDUX IN REACT COMPONENTS

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,3 +1,29 @@
-const Book = () => <li>First Book</li>;
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { removeBook } from '../redux/books/booksSlice';
+
+const Book = ({
+  id, title, author, category,
+}) => {
+  const disptch = useDispatch();
+
+  return (
+    <li>
+      <div>
+        <h3>{title}</h3>
+        <p>{author}</p>
+        <p>{category}</p>
+      </div>
+      <button type="button" onClick={() => disptch(removeBook(id))}>remove a book</button>
+    </li>
+  );
+};
+
+Book.propTypes = {
+  id: PropTypes.node.isRequired,
+  author: PropTypes.node.isRequired,
+  category: PropTypes.node.isRequired,
+  title: PropTypes.node.isRequired,
+};
 
 export default Book;
