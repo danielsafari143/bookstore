b0adaa01f25b76487dd7af5c64485e125caa9588
danielsafari143 <danielsafari143@gmail.com>
2023-06-14 02:11:46 +0200

    FIX ERRORS

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,12 +1,14 @@
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { nanoid } from 'nanoid';
+import Alert from './Alert';
 import { creatBook } from '../redux/books/booksSlice';
 
 const Forms = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const dispatch = useDispatch();
+  const [show, setShow] = useState(false);
 
   return (
     <div className="forme">
@@ -19,15 +21,24 @@ const Forms = () => {
             className="btn-cls"
             type="button"
             value="ADD BOOK"
-            onClick={() => dispatch(creatBook({
-              item_id: nanoid(),
-              title,
-              author,
-              category: 'Fiction',
-            }))}
+            onClick={() => {
+              if (title.length > 1 && author.length > 1) {
+                setShow(false);
+                return dispatch(creatBook({
+                  item_id: nanoid(),
+                  title,
+                  author,
+                  category: 'Fiction',
+                }));
+              }
+              return setShow((previousValue) => !previousValue);
+            }}
           />
         </form>
       </div>
+      {
+        show && <Alert />
+      }
     </div>
   );
 };
